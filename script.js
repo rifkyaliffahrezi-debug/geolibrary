186280fbe164790adc2efeba1918a34fe0bf050d
Geoolibrary <rifkyaliffahrezi@gmail.com>
2026-06-25 00:12:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -214,3 +214,23 @@ L.marker(wilayah.koordinat)
 .bindPopup(wilayah.potensi);
 
 });
+
+const acc = document.querySelectorAll(".accordion");
+
+acc.forEach(btn => {
+
+btn.addEventListener("click", function(){
+
+this.classList.toggle("active");
+
+const panel = this.nextElementSibling;
+
+if(panel.style.display === "block"){
+panel.style.display = "none";
+}else{
+panel.style.display = "block";
+}
+
+});
+
+});
